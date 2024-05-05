c313ed2be4c56c56022dace30646eeb3f576ab4b
Alekaras91 <papagiannis.a.999@gmail.com>
2024-05-05 01:37:11 +0100

    Fixing the truncate proble by truncating both tables at the same time in cohorts.sql file

--- seeds/cohorts.sql
+++ seeds/cohorts.sql
@@ -1,9 +1,7 @@
-TRUNCATE TABLE cohorts RESTART IDENTITY;
+TRUNCATE TABLE cohorts, students RESTART IDENTITY;
 
 INSERT INTO cohorts (name, starting_date) VALUES ('April 23', '10/04/2023');
 INSERT INTO cohorts (name, starting_date) VALUES ('June 23', '12/06/2023');
-
-TRUNCATE TABLE students RESTART IDENTITY;
 
 INSERT INTO students (name, cohort_id) VALUES ('Ana Ivanovic', 1);
 INSERT INTO students (name, cohort_id) VALUES ('Maria Sharapova', 2);
--- spec/cohort_repository_spec.rb
+++ spec/cohort_repository_spec.rb
@@ -2,7 +2,7 @@ require 'cohort_repository'
 
 RSpec.describe CohortRepository do
   def reset_cohorts_table
-    seed_sql = File.read('seeds/students.sql')
+    seed_sql = File.read('seeds/cohorts.sql')
     connection = PG.connect({ host: '127.0.0.1', dbname: 'student_directory_2_test' })
     connection.exec(seed_sql)
   end  
